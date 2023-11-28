b50b6aad38008b1be13b8fe8ea7e94560d8ef2e4
3zzinup <wintiger98@gmail.com>
2023-11-28 10:41:07 +0900

    java tutorial

---
+++ src/operator/Operator1.java
@@ -0,0 +1,24 @@
+package operator;
+
+public class Operator1 {
+
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 2;
+
+    int sum = a + b;
+    System.out.println("sum = " + sum);
+
+    int diff = a - b;
+    System.out.println("diff = " + diff);
+
+    int multi = a * b;
+    System.out.println("multi = " + multi);
+
+    int div = a / b;
+    System.out.println("div = " + div);
+
+    int mod = a % b;
+    System.out.println("mod = " + mod);
+  }
+}
---
+++ src/operator/ex/OperatorEx3.java
@@ -0,0 +1,10 @@
+package operator.ex;
+
+public class OperatorEx3 {
+
+  public static void main(String[] args) {
+    int score = 80;
+    boolean boolVar = (score >= 80 && score <= 100 ? true : false);
+    System.out.println("boolVar = " + boolVar);
+  }
+}
