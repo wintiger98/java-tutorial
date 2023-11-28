ff9fb68542c4458fd13428c32f7b3ccf51b732e0
3zzinup <wintiger98@gmail.com>
2023-11-28 10:20:49 +0900

    java tutorial

---
+++ src/variable/Var7.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var7 {
+
+  public static void main(String[] args) {
+    int a = 100;
+    double b = 10.5;
+    boolean c = true;
+    char d = 'A';
+    String e = "Hello java";
+
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+    System.out.println("c = " + c);
+    System.out.println("d = " + d);
+    System.out.println("e = " + e);
+  }
+}
---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3 {
+
+  public static void main(String[] args) {
+    long num1 = 10000000000L;
+    boolean is_true = true;
+    System.out.println(num1);
+    System.out.println(is_true);
+  }
+}
